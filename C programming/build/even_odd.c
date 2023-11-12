006670ab7810a3bf2fe353ae9eff75c3d826d5b5
Edwin Waweru <120776412+waweruedwin8@users.noreply.github.com>
2023-11-12 18:48:43 +0300

    Update even_odd.c

---
+++ C programming/build/even_odd.c
@@ -0,0 +1,12 @@
+// this code detects if a number is even or odd
+#include <stdio.h>
+
+int main()
+{
+    int number;
+    printf(" Enter a whole Number: ");
+    scanf("%d", &number);
+    
+    (number % 2 == 0) ? printf("This is an even number") : printf("This is an odd number");
+    return 0;
+}
