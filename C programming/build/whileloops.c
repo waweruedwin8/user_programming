7b07c490925c8317d9fb999284ae3608405a35fc
waweruedwin8 <waweruedwin8@gmail.com>
2023-11-17 17:17:57 +0300

    innit

---
+++ C programming/build/whileloops.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+ *  //syntax
+ * 
+ *  while (condition) 
+ * {
+ * statements
+ * }
+*/
+int main ()
+{
+    int count =1;
+    
+    while (count < 5 )
+    {
+        printf(" while loop in c \n");
+        count = count+1;
+    }
+}
