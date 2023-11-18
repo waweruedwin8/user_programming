2ed35f072cda72ea61a0972d90ae01234b22817d
waweruedwin8 <waweruedwin8@gmail.com>
2023-11-18 12:05:39 +0300

    innit

---
+++ C programming/build/do_while_loop.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int count =1;
+    do {
+            printf ("%d \n", count);
+            count = count + 1;
+    } while (count < 5);
+    
+    return 0;
+}
