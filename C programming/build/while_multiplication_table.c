236a7272a957af2cb51b01c07f077e4b11653260
waweruedwin8 <waweruedwin8@gmail.com>
2023-11-18 12:58:07 +0300

    innit

---
+++ C programming/build/while_multiplication_table.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+// a program that impliments a multiplication  table that multplies numbers backward using a while loop 
+
+int main()
+{
+    int count = 10;
+    int number;
+    int product;
+
+    printf(" Enter a number to be  multiplied:  ");
+    scanf("%d", &number);
+
+    while (count >= 1)
+    {
+        int product = number * count ;
+        printf("%d * %d = %d\n",number,count ,product);
+        count--;
+    }
+    return 0;
+}
