218feb435a4c9e0abcc05f3a4633a9112e6e98b8
waweruedwin8 <waweruedwin8@gmail.com>
2023-11-28 16:27:15 +0300

    innit

---
+++ python/distance_converter.py
@@ -0,0 +1,4 @@
+# a programme that converts distance in km to miles
+kilometer = float(input("Enter distance in kilometers: "))
+distance = kilometer *  0.621371
+print("The distance in miles is: ",distance)
