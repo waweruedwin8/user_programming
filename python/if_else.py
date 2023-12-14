5ffaf3e1af72ba0e2efbf71918a744710eadc39a
waweruedwin8 <waweruedwin8@gmail.com>
2023-12-14 13:18:44 +0300

    python

---
+++ python/if_else.py
@@ -0,0 +1,7 @@
+score = int(input("Enter your score: "))
+if score >= 50:
+    print ("your Score is: ",score) 
+    print ("you have passed the exams, congulatulations" )
+else:
+    print ("your Score is: ",score) 
+    print ("you have not met the minimum theresh hold in the exams, please make another attempt")
