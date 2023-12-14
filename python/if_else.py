820898a826f2f54706b6de934400a97dc0535d1c
waweruedwin8 <waweruedwin8@gmail.com>
2023-12-14 13:27:04 +0300

    python

--- python/if_else.py
+++ python/if_else.py
@@ -1,5 +1,8 @@
+# a program that checks if the student has passed the exams based on a set pass mark
 score = int(input("Enter your score: "))
-if score >= 50:
+if score >= 100 or score < 0:
+    print("You enterd an Invalid score")
+elif score >= 50:
     print ("your Score is: ",score) 
     print ("you have passed the exams, congulatulations" )
 else:
