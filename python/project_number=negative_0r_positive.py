00d80f57f3f1506b37b09c590c5c1185ee06862d
waweruedwin8 <waweruedwin8@gmail.com>
2023-12-15 22:41:29 +0300

    python

---
+++ python/project_number=negative_0r_positive.py
@@ -0,0 +1,8 @@
+# a program that checks whether if a number that the user input  is +tve or -tve
+number = float(input("Enter a number: "))
+if number == 0:
+    print("the number is zero")
+elif number > 0.1 :
+    print("The number is positive")
+else:
+    print("The number is negative")
