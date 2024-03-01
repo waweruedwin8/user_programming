ade00bd5414589f347d36d90feee1c67f392df41
waweruedwin8 <waweruedwin8@gmail.com>
2024-03-01 18:44:33 +0300

    loops

--- python/cat.py
+++ python/cat.py
@@ -1,5 +1,5 @@
-# a programe that impliments while loop
-i = 3
-while i !=0:
-    i -= 1 # you can  also use i= i-1
+# a programme that impliments while loop to imliment cat sound
+i = 0
+while i < 5:
+    i += 1 # you can  also use i= i-1
     print("meow")
---
+++ python/conditionals_loops.py
@@ -0,0 +1,9 @@
+n = input("whats your name? ")
+print("hello ", n)
+while True:
+    r = int(input("whats r? "))
+    if r > 0:
+        break
+
+for h in range(r):
+        print("hello ", n)
